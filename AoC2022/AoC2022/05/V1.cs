8757c5f030cbcab0294077a7d8e54d18eca503ae
Dave Senn <dave.jon.senn@gmail.com>
2022-12-05 10:44:20 +0100

    .

---
+++ AoC2022/AoC2022/05/V1.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+[SuppressMessage( "ReSharper", "UnusedType.Global" )]
+public sealed partial class Day5
+{
+    [GeneratedRegex( @"move (.*) from (.*) to (.*)", RegexOptions.IgnoreCase | RegexOptions.Compiled )]
+    private static partial Regex MoveRegex();
+
+    private static void V1()
+    {
+        var instructionParts = Input.Split( $"{Environment.NewLine}{Environment.NewLine}" );
+        Stack<Char>[]? stacks = null;
+        foreach ( var line in instructionParts[0]
+                              .Split( Environment.NewLine )
+                              .Reverse()
+                              .Skip( 1 ) )
+        {
+            stacks ??= Enumerable.Range( 0,
+                                         line
+                                             .Chunk( 4 )
+                                             .Count() )
+                                 .Select( _ => new Stack<Char>() )
+                                 .ToArray();
+
+            foreach ( var (stack, container) in stacks
+                                                .Zip( line.Chunk( 4 ) )
+                                                .Where( x => x.Second[1] != ' ' ) )
+                stack.Push( container[1] );
+        }
+
+        foreach ( var matches in instructionParts[1]
+                                 .Split( Environment.NewLine )
+                                 .Select( x => MoveRegex()
+                                              .Match( x ) ) )
+        {
+            var source = stacks![Int32.Parse( matches.Groups[2]
+                                                     .Value ) - 1];
+            var target = stacks[Int32.Parse( matches.Groups[3]
+                                                    .Value ) - 1];
+            for ( var i = 0; i < Int32.Parse( matches.Groups[1]
+                                                     .Value ); i++ )
+                target.Push( source.Pop() );
+        }
+
+        var result = new String( stacks!.Select( x => x.Pop() )
+                                        .ToArray() );
+        Console.WriteLine( $"Container sequence: {result}" );
+    }
+}
---
+++ AoC2022/AoC2022/05/V2.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage( "ReSharper", "UnusedType.Global" )]
+public sealed partial class Day5
+{
+    private static void V2()
+    {
+        var instructionParts = Input.Split( $"{Environment.NewLine}{Environment.NewLine}" );
+        Stack<Char>[]? stacks = null;
+        foreach ( var line in instructionParts[0]
+                              .Split( Environment.NewLine )
+                              .Reverse()
+                              .Skip( 1 ) )
+        {
+            stacks ??= Enumerable.Range( 0,
+                                         line
+                                             .Chunk( 4 )
+                                             .Count() )
+                                 .Select( _ => new Stack<Char>() )
+                                 .ToArray();
+
+            foreach ( var (stack, container) in stacks
+                                                .Zip( line.Chunk( 4 ) )
+                                                .Where( x => x.Second[1] != ' ' ) )
+                stack.Push( container[1] );
+        }
+
+        foreach ( var matches in instructionParts[1]
+                                 .Split( Environment.NewLine )
+                                 .Select( x => MoveRegex()
+                                              .Match( x ) ) )
+        {
+            var source = stacks![Int32.Parse( matches.Groups[2]
+                                                     .Value ) - 1];
+            var target = stacks[Int32.Parse( matches.Groups[3]
+                                                    .Value ) - 1];
+
+            foreach ( var container in Enumerable
+                                       .Range( 0,
+                                               Int32.Parse( matches.Groups[1]
+                                                                   .Value ) )
+                                       .Select( x => source.Pop() )
+                                       .Reverse() )
+                target.Push( container );
+        }
+
+        var result = new String( stacks!.Select( x => x.Pop() )
+                                        .ToArray() );
+        Console.WriteLine( $"Container sequence: {result} Mover 9001" );
+    }
+}
