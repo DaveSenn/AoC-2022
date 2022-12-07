6cad3a53360f4e5c16175245b73f7f1eb66e979b
Dave Senn <dave.jon.senn@gmail.com>
2022-12-07 13:29:08 +0100

    .

---
+++ AoC2022/07/Day7.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage( "ReSharper", "UnusedType.Global" )]
+public sealed partial class Day7 : IDay
+{
+    private static Dictionary<String, UInt32> GetDirSizes()
+    {
+        var lines = Input.SplitLines2();
+
+        var dirs = new Dictionary<String, UInt32>( 256 );
+        var currentLocation = String.Empty;
+        var dirLocations = new Stack<String>( 16 );
+        while ( lines.MoveNext() )
+        {
+            var currentLine = lines.Current;
+
+            if ( currentLine.Length == 7
+                 && currentLine[0] == '$'
+                 && currentLine[1] == ' '
+                 && currentLine[2] == 'c'
+                 && currentLine[3] == 'd'
+                 && currentLine[4] == ' '
+                 && currentLine[5] == '.'
+                 && currentLine[6] == '.' )
+            {
+                dirLocations.Pop();
+                var end = currentLocation.LastIndexOf( '/' );
+                currentLocation = currentLocation[..( end + 1 )];
+                continue;
+            }
+
+            if ( currentLine.Length >= 6
+                 && currentLine[0] == '$'
+                 && currentLine[1] == ' '
+                 && currentLine[2] == 'c'
+                 && currentLine[3] == 'd' )
+            {
+                var dirName = currentLine[5..];
+                currentLocation = $"{currentLocation}/{dirName}";
+                dirLocations.Push( currentLocation );
+                continue;
+            }
+
+            // ReSharper disable once InvertIf
+            if ( Char.IsNumber( currentLine[0] ) )
+            {
+                var size = UInt32.Parse( currentLine[..currentLine.IndexOf( ' ' )] );
+
+                foreach ( var dir in dirLocations )
+                    if ( dirs.TryGetValue( dir, out var subTotal ) )
+                        dirs[dir] = subTotal + size;
+                    else
+                        dirs[dir] = size;
+            }
+        }
+
+        return dirs;
+    }
+
+    #region Implementation of IDay
+
+    public ValueTask Run()
+    {
+        var dirs = GetDirSizes();
+
+        UInt32 result0 = 0;
+        foreach ( var (_, x) in dirs )
+            if ( x < 100_000 )
+                result0 += x;
+
+        var current = 70000000 - dirs.Max( x => x.Value );
+        var result1 = dirs
+                      .Where( x => x.Value + current >= 30000000 )
+                      .Min( x => x.Value );
+
+        Console.WriteLine( $"Index of first unique sequence: {result0}" );
+        Console.WriteLine( $"Index of first unique sequence: {result1}" );
+
+        return ValueTask.CompletedTask;
+    }
+
+    public Int32 Day => 7;
+
+    #endregion
+}
---
+++ AoC2022/07/StringEx.cs
@@ -0,0 +1,48 @@
+﻿public static class StringEx
+{
+    public static LineSplitEnumerator SplitLines2( this String str ) =>
+        new(str.AsSpan());
+
+    public ref struct LineSplitEnumerator
+    {
+        private ReadOnlySpan<Char> _str;
+
+        public LineSplitEnumerator( ReadOnlySpan<Char> str )
+        {
+            _str = str;
+            Current = default;
+        }
+
+        public Boolean MoveNext()
+        {
+            var span = _str;
+            if ( span.Length == 0 )
+                return false;
+
+            var index = span.IndexOfAny( '\r', '\n' );
+            if ( index == -1 )
+            {
+                _str = ReadOnlySpan<Char>.Empty;
+                Current = span;
+                return true;
+            }
+
+            if ( index < span.Length - 1 && span[index] == '\r' )
+            {
+                var next = span[index + 1];
+                if ( next == '\n' )
+                {
+                    Current = span[..index];
+                    _str = span[( index + 2 )..];
+                    return true;
+                }
+            }
+
+            Current = span[..index];
+            _str = span[( index + 1 )..];
+            return true;
+        }
+
+        public ReadOnlySpan<Char> Current { get; private set; }
+    }
+}
